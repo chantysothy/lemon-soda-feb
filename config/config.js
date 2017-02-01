4e897d758c028433a64bc23d31e888d69d3c7e8b
lemon-soda <edward.varghese@gmail.com>
2017-02-01 11:59:24 +0530

    updating Jan's work

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+	twitter: {
+        'consumer_key': 'zR30W1z6cTQfYKaeMMrUdbXKm'
+        , 'consumer_secret': 'W3k4tedpDKJ0hM6Hnn2I4hAHWag1INFw2ipaBOhgYBqt5zaxL2'
+        , 'access_token': '103259010-9AtPODqSaurMAOaIiIfnJrr14TloFKXgx9wElaop'
+        , 'access_token_secret': 'pJWeIvX69HhXr3f6WicjEbZfhWLDezcjUzcXbVBWsyEF5'
+        , 'redirect_url': 'http://localhost:1337/auth/twitter/callback'
+        // for updating to git once again and again
+        //,
+        //bearer_token:''
+        //test
+	},
+    facebookAuth: {
+        'client_id': '334582780223007'
+        , 'client_secret': '85f5abe5d8390c9134c9dd84befa26ed'
+        , 'redirect_uri': 'http://localhost:1337/auth/facebook/callback'
+        , 'code': 'EAAEwTRZCDXh8BAL5AtzF9Lt5VAu2aKYnGHLIz7h0MmkpSogLyQ4nK7lH4HNiGV13PJRVWKd3vZAu4MxZAZCmkqkxIasQZAcZByhwvvd5eqyYDlMS7M04uagHAXa9mNwTrZCtPxYhRDotE6MlinTmhtQBCnTsE8JHcJUyZB0ZChkTidAZDZD'
+        //,
+        //bearer_token:''
+        //test
+    }
+
+};
